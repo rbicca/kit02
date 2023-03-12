ceaa1ea1588386a0edbd3c863ea3cd63518074a2
Ronaldo Melchiades Bicca <ronaldo.bicca@gmail.com>
2023-03-11 21:19:42 -0300

    One more

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,13 +7,18 @@ export const handle:Handle = async ({event, resolve}) => {
     console.log('Disparando hook', event);
 
     //Exemplo de override
-    const { url } = event;
+    const { url, locals ,cookies, isDataRequest} = event;
     // if (url.pathname === '/products'){
     //     return new Response('Rota interceptada');
     // }
 
+    //Tudo que colocarmos no locals, vai estar disponivel em todas
+    // as outras ServerLoad Functions
+    if(!isDataRequest && !url.pathname.startsWith('/api')){
+        const token = cookies.get('token');
+        locals.user = token ?  { name: 'kuka', id: 1 } : undefined;
+    }
 
-    
     const response = await resolve(event);
     
     //Adicionar headers
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,12 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = ({cookies}) => {
+export const load: LayoutServerLoad = ({cookies, locals}) => {
     const token = cookies.get('token');
 
     //nesse momento obteríamos o usuário do token
+
+    //Agora que implementamos um hookie - podemos pegar o user que espetamos em locals
+    console.log('do locals hookie', locals);
 
     const user = { name: 'kuka', id: 1 };
     //console.log('em token temos ', token);
